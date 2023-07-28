a14230dd35346dab7a8317b40ba20ca68e5fb999
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-07-28 13:31:23 +0900

    s/Drill2/Excite/

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -772,7 +772,7 @@ jQuery(function($){
 			$('#sound_select option[value=""]').text('(なし)');
 			$('#sound_select option[value="Cattish"]').text('ネコっぽい');
 			$('#sound_select option[value="Drill"]').text('ドリル');
-			$('#sound_select option[value="Drill2"]').text('ドリル 2');
+			$('#sound_select option[value="Excite"]').text('興奮');
 			$('#sound_select option[value="Horror"]').text('恐怖');
 			$('#sound_select option[value="Hunting"]').text('狩人');
 			$('#sound_select option[value="Lonely"]').text('さびしさ');
@@ -839,7 +839,7 @@ jQuery(function($){
 			$('#sound_select option[value=""]').text('(无)');
 			$('#sound_select option[value="Cattish"]').text('像猫一样');
 			$('#sound_select option[value="Drill"]').text('钻头');
-			$('#sound_select option[value="Drill2"]').text('钻头 2');
+			$('#sound_select option[value="Excite"]').text('激发');
 			$('#sound_select option[value="Horror"]').text('害怕');
 			$('#sound_select option[value="Hunting"]').text('猎人');
 			$('#sound_select option[value="Lonely"]').text('孤独');
@@ -906,7 +906,7 @@ jQuery(function($){
 			$('#sound_select option[value=""]').text('(無)');
 			$('#sound_select option[value="Cattish"]').text('像貓一樣');
 			$('#sound_select option[value="Drill"]').text('鑽頭');
-			$('#sound_select option[value="Drill2"]').text('鑽頭 2');
+			$('#sound_select option[value="Excite"]').text('激發');
 			$('#sound_select option[value="Horror"]').text('害怕');
 			$('#sound_select option[value="Hunting"]').text('獵人');
 			$('#sound_select option[value="Lonely"]').text('孤獨');
@@ -973,7 +973,7 @@ jQuery(function($){
 			$('#sound_select option[value=""]').text('(없음)');
 			$('#sound_select option[value="Cattish"]').text('고양이 같은');
 			$('#sound_select option[value="Drill"]').text('드릴');
-			$('#sound_select option[value="Drill2"]').text('드릴 2');
+			$('#sound_select option[value="Excite"]').text('일으키다');
 			$('#sound_select option[value="Horror"]').text('공포');
 			$('#sound_select option[value="Hunting"]').text('사냥꾼');
 			$('#sound_select option[value="Lonely"]').text('녹슬다');
@@ -1040,7 +1040,7 @@ jQuery(function($){
 			$('#sound_select option[value=""]').text('(Nessuno)');
 			$('#sound_select option[value="Cattish"]').text('Simile a un gatto');
 			$('#sound_select option[value="Drill"]').text('Trapano');
-			$('#sound_select option[value="Drill2"]').text('Trapano 2');
+			$('#sound_select option[value="Excite"]').text('Eccitare');
 			$('#sound_select option[value="Horror"]').text('Paura');
 			$('#sound_select option[value="Hunting"]').text('A caccia');
 			$('#sound_select option[value="Lonely"]').text('Solitudine');
@@ -1107,7 +1107,7 @@ jQuery(function($){
 			$('#sound_select option[value=""]').text('(Kein)');
 			$('#sound_select option[value="Cattish"]').text('Katzenartig');
 			$('#sound_select option[value="Drill"]').text('Bohren');
-			$('#sound_select option[value="Drill2"]').text('Bohren 2');
+			$('#sound_select option[value="Excite"]').text('Anregen');
 			$('#sound_select option[value="Horror"]').text('Furcht');
 			$('#sound_select option[value="Hunting"]').text('Jagd');
 			$('#sound_select option[value="Lonely"]').text('Einsamkeit');
@@ -1174,7 +1174,7 @@ jQuery(function($){
 			$('#sound_select option[value=""]').text('(None)');
 			$('#sound_select option[value="Cattish"]').text('Cattish');
 			$('#sound_select option[value="Drill"]').text('Drill');
-			$('#sound_select option[value="Drill2"]').text('Drill 2');
+			$('#sound_select option[value="Excite"]').text('Excite');
 			$('#sound_select option[value="Horror"]').text('Horror');
 			$('#sound_select option[value="Hunting"]').text('Hunting');
 			$('#sound_select option[value="Lonely"]').text('Lonely');
