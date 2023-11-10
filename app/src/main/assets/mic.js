c66a84fcc903252fed98a5140d12a265081a2f8e
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-11-10 10:50:31 +0900

    coding rules 2

--- app/src/main/assets/mic.js
+++ app/src/main/assets/mic.js
@@ -1,49 +1,52 @@
+// mic.js --- マイク入力からスピーカー出力をするだけのライブラリ。
+// Copyright (C) 2023 katahiromz.
+
 var mic_audio = null;
 var mic_microphone = null;
 
 const mic_setup = function(){
-  navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
-    getUserMedia: function(c){
-      return new Promise(function(y, n){
-        (navigator.mozGetUserMedia || navigator.webkitGetUserMedia).call(navigator, c, y, n);
-      });
-    }
-  } : null);
-  if (!navigator.mediaDevices){
-    console.log('!navigator.mediaDevices');
-    return false;
-  }
-  return true;
+	navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+		getUserMedia: function(c){
+			return new Promise(function(y, n){
+				(navigator.mozGetUserMedia || navigator.webkitGetUserMedia).call(navigator, c, y, n);
+			});
+		}
+	} : null);
+	if (!navigator.mediaDevices){
+		console.log('!navigator.mediaDevices');
+		return false;
+	}
+	return true;
 }
 
 const mic_connect = function(){
-  if (mic_audio){
-    mic_microphone.connect(mic_audio.destination);
-    return true;
-  }
-  var constraints = window.constraints = { 
-    audio: {
-      autoGainControl: true,
-      channelCount: 1,
-      echoCancellation: true,
-      noiseSuppression: false,
-      volume: {ideal: 3.0, min: 1.0},
-      echoCancellationType : 'system'
-    }
-  };
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
-    mic_audio = new AudioContext();
-    mic_microphone = mic_audio.createMediaStreamSource(stream);
-    mic_microphone.connect(mic_audio.destination);
-  }).catch(function(err){
-    console.log(err.message);
-  });
-  return true;
+	if (mic_audio){
+		mic_microphone.connect(mic_audio.destination);
+		return true;
+	}
+	var constraints = window.constraints = { 
+		audio: {
+			autoGainControl: true,
+			channelCount: 1,
+			echoCancellation: true,
+			noiseSuppression: false,
+			volume: {ideal: 3.0, min: 1.0},
+			echoCancellationType : 'system'
+		}
+	};
+	navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+		mic_audio = new AudioContext();
+		mic_microphone = mic_audio.createMediaStreamSource(stream);
+		mic_microphone.connect(mic_audio.destination);
+	}).catch(function(err){
+		console.log(err.message);
+	});
+	return true;
 }
 
 const mic_disconnect = function(){
-  if (mic_microphone){
-    mic_microphone.disconnect();
-  }
-  return true;
+	if (mic_microphone){
+		mic_microphone.disconnect();
+	}
+	return true;
 }
