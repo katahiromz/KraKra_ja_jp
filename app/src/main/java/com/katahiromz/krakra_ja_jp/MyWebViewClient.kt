1b18326a6d7bef1d3eb96c83777d191a06e1f26a
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-01-21 19:43:08 +0900

    improve face recognition 7

--- app/src/main/java/com/katahiromz/krakra_ja_jp/MyWebViewClient.kt
+++ app/src/main/java/com/katahiromz/krakra_ja_jp/MyWebViewClient.kt
@@ -31,10 +31,6 @@ class MyWebViewClient(private val listener: Listener) : WebViewClient() {
                 view.loadUrl(url)
                 return true;
             }
-            if (url == "https://raw.githubusercontent.com/nenadmarkus/pico/c2e81f9d23cc11d1a612fd21e4f9de0921a5d0d9/rnt/cascades/facefinder") {
-                view.loadUrl(url)
-                return true;
-            }
         }
         return true
     }
