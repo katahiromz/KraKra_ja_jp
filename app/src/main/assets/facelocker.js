54269d2c0c73d6953986afe57d7bfc680ae1ac8b
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-01-21 19:26:38 +0900

    improve face recognition 6

--- app/src/main/assets/facelocker.js
+++ app/src/main/assets/facelocker.js
@@ -305,8 +305,10 @@ const facelocker = function(canvas, on_lock){
 
 		// Initialize pico.js face detector
 		self.update_memory = pico.instantiate_detection_memory(5); // we will use the detecions of the last 5 frames
-		let cascadeurl = 'https://raw.githubusercontent.com/nenadmarkus/pico/c2e81f9d23cc11d1a612fd21e4f9de0921a5d0d9/rnt/cascades/facefinder';
-		fetch(cascadeurl).then(function(response){
+		let url = './facefinder';
+		if(location.protocol == 'file:')
+			url = 'https://katahiromz.github.io/saimin/facefinder';
+		fetch(url).then(function(response){
 			response.arrayBuffer().then(function(buffer){
 				let bytes = new Int8Array(buffer);
 				self.classify_region = pico.unpack_cascade(bytes);
