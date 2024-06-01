159e6395cb22ffd0e0c306677b4c746a2be4dd38
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2024-06-01 10:21:11 +0900

    fixed pic6? 8

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -2170,17 +2170,12 @@ document.addEventListener('DOMContentLoaded', function(){
 				let x1 = sai_touch_position[0] + sai_coin_img.width * 0.6;
 				let y0 = sai_touch_position[1] - sai_coin_img.width * 0.6;
 				let y1 = sai_touch_position[1] + sai_coin_img.width * 0.6;
-				for (let i = 0; i < 2; ++i){
-					if(true && i == 0){
+				for (let i = 0; i < 3; ++i){
+					if(true){ // デバッグ用。
 						ctx.fillStyle = 'black';
 						ctx.fillRect(x0, y0, x1 - x0, y1 - y0);
 						ctx.fillStyle = 'green';
 						ctx.fillRect(qx - 5, qy - 5, 10, 10);
-						ctx.font = `12px san-serif`;
-						ctx.textAlign = 'left';
-						ctx.textBaseline = 'top';
-						ctx.fillStyle = "white";
-						ctx.fillText(`(${px}, ${py}, ${dx}, ${dy}, ${Math.trunc(qx)}, ${Math.trunc(qy)}, ${sai_touch_position[0]}, ${sai_touch_position[1]})`, px, py);
 					}
 					if(x0 <= qx && qx <= x1 && y0 <= qy && qy <= y1){
 						sai_touching_coin = true;
@@ -2190,9 +2185,21 @@ document.addEventListener('DOMContentLoaded', function(){
 						break;
 					}else{ // 画面２分割。
 						if(dx >= dy){ // 横長。
-							qx += dx;
+							if(i == 0){
+								x0 += dx;
+								x1 += dx;
+							}else{
+								x0 -= 2 * dx;
+								x1 -= 2 * dx;
+							}
 						}else{ // 縦長。
-							qy += dy;
+							if(i == 0){
+								y0 += dy;
+								y1 += dy;
+							}else{
+								y0 -= 2 * dy;
+								y1 -= 2 * dy;
+							}
 						}
 					}
 				}
