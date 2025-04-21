c1798761b9e8a26087cc0ad90b73988537833870
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-04-21 11:26:10 +0900

    improve 2

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -1,7 +1,7 @@
 // 催眠アプリ「催眠くらくら」のJavaScriptのメインコード。
 // 暗号名はKraKra。
 
-const sai_VERSION = '3.7.8'; // KraKraバージョン番号。
+const sai_VERSION = '3.7.7'; // KraKraバージョン番号。
 const sai_DEBUGGING = false; // デバッグ中か？
 let sai_FPS = 0; // 実測フレームレート。
 
@@ -532,7 +532,10 @@ document.addEventListener('DOMContentLoaded', function(){
 			android.startVibrator(sai_id_range_vibrator_strength.value.toString());
 			console.log("SAI_vibrator_start: " + sai_id_range_vibrator_strength.value);
 		}catch(error){ // Androidではない。
-			console.log("SAI_vibrator_start: failed");
+			if('vibrate' in navigator){
+				navigator.vibrate(0); // 振動を停止。
+				navigator.vibrate([20 * 60 * 1000]); // 20分間振動。
+			}
 		}
 	}
 
@@ -542,7 +545,9 @@ document.addEventListener('DOMContentLoaded', function(){
 			android.stopVibrator();
 			console.log("SAI_vibrator_stop: stopped");
 		}catch(error){ // Androidではない。
-			console.log("SAI_vibrator_stop: failed");
+			if('vibrate' in navigator){
+				navigator.vibrate(0); // 振動を停止。
+			}
 		}
 	}
 
@@ -4282,6 +4287,13 @@ document.addEventListener('DOMContentLoaded', function(){
 				item.classList.add('sai_class_invisible');
 			}
 		}
+		// バイブレーターが有効でなければバイブレーターのみの要素を隠す。
+		if(!SAI_is_android_app() && !('vibrate' in navigator)){
+			let items = document.getElementsByClassName('sai_class_vibrator_only');
+			for(let item of items){
+				item.classList.add('sai_class_invisible');
+			}
+		}
 
 		// ローカルストレージに応じて処理を行う。
 		let saiminUserAccepted = localStorage.getItem('saiminUserAccepted');
@@ -4356,19 +4368,6 @@ document.addEventListener('DOMContentLoaded', function(){
 
 		// キーボード操作を実装。
 		SAI_register_key_bindings();
-
-		// スマホ・タブレットでのズームを禁止する。
-		document.addEventListener('touchmove', function(e){
-			if(e.scale !== 1){
-				e.preventDefault();
-			}
-		},{ passive: false });
-		// Ctrl+ホイールでのズームを禁止する。
-		document.addEventListener('wheel', function(e){
-			if(e.ctrlKey){
-				e.preventDefault();
-			}
-		},{ passive: false });
 	}
 
 	// メイン関数を呼び出す。
