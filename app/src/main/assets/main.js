87a58bf998a92511c4f993162a00368b707958bb
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-04-22 11:00:36 +0900

    fix

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -83,7 +83,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 	// Androidか？
 	const SAI_is_android_app = function(){
-		return navigator.userAgent.indexOf('/Android/') != -1;
+		return navigator.userAgent.indexOf('Android') != -1;
 	}
 
 	// Androidのネイティブアプリか？
