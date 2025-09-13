7811f01a426f6f082b1783814872158fabef4727
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2025-09-13 19:09:27 +0900

    update web2assets.sh 2

--- web2assets.sh
+++ web2assets.sh
@@ -2,7 +2,6 @@
 cp -fr ../katahiromz.github.io/saimin/* ./app/src/main/assets/
 rm -fr ./app/src/main/assets/README.md
 rm -fr ./app/src/main/assets/LICENSE.txt
-rm -fr ./app/src/main/assets/favicon.ico
 rm -fr ./app/src/main/assets/mic_test.html
 rm -fr ./app/src/main/assets/service-worker.js
 rm -fr ./app/src/main/assets/sw.js
